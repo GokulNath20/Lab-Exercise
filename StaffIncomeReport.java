a8d9a00733d5cf58883828e71266634db3dfdb21
GokulNath20 <61192087+GokulNath20@users.noreply.github.com>
2020-03-16 15:09:33 +0800

    Add files via upload

--- StaffIncomeReport.java
+++ StaffIncomeReport.java
@@ -38,5 +38,87 @@ public class StaffIncomeReport{//class name
    }
    
  }  
+ 
+class Income{
+
+   private String IncomeType;
+   private double amount;
+   private double tax;
+   private double totalTax;
+   
+   public Income(String IncomeType,double amount,double tax){
+   
+   this.IncomeType = IncomeType;
+   this.amount = amount;
+   this.tax = tax;
+   
+   }
+   
+   public String getIncomeType(){
+          return IncomeType; 
+   }
+
+   
+   public double getAmount(){
+         return amount;
+   }
+   
+   public double getTax(){
+         return tax;
+   }
+   
+   public double getTotalTax(){
+   
+   totalTax = (tax/100)*amount;
+   return totalTax;
+   }
+   
+}
+   
+class Staff{
+
+   private String name;
+   private String id;
+   private Income income;
+   double netIncome;
    
    
+   public Staff(String name,String id,Income income){
+   
+   this.income=income;
+   this.name=name;
+   this.id=id;
+
+   }
+   
+   public String getName(){
+      return name;
+   }
+   
+   public String getId(){
+      return id;
+   }
+   
+   double calculateNetIncome(){
+      netIncome = income.getAmount() - income.getTotalTax();
+      return netIncome;
+   }
+   
+   public void displayStaffInfo(){
+      System.out.println("Name: "+ name);
+      System.out.println("ID: "+ id);
+   }
+   
+     
+   public void displayIncomeInfo(){
+   
+      System.out.println("Income type: "+ income.getIncomeType() );
+      System.out.printf("Income amount:RM %.2f", income.getAmount() );
+      System.out.println("\nTax: "+ income.getTax() );
+      System.out.printf("Total tax:RM %.2f", income.getTotalTax() );
+      System.out.printf("\nNet income:RM %.2f", calculateNetIncome() );
+   
+   }
+ }  
+   
+      
